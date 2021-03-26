d921d57a5b4f10c995567f4341b03fc5e8cec635
Shubham Mehrotra <shubh0896m@gmail.com>
2021-03-26 23:12:54 +0530

    updated gitignore

---
+++ config/concord.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'modules' => [
+        /**
+         * Example:
+         * VendorA\ModuleX\Providers\ModuleServiceProvider::class,
+         * VendorB\ModuleY\Providers\ModuleServiceProvider::class
+         *
+         */
+    ],
+    'register_route_models' => true
+];
