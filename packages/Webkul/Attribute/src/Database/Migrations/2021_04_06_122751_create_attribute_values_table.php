3c70e98adb4670ff7e522f046f68b1d85ace0783
jitendra <jitendra@webkul.com>
2021-04-07 12:25:27 +0530

    Attribute value migrations added

---
+++ packages/Webkul/Attribute/src/Database/Migrations/2021_04_06_122751_create_attribute_values_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributeValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_values', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('entity_type')->default('leads');
+            $table->text('text_value')->nullable();
+            $table->boolean('boolean_value')->nullable();
+            $table->integer('integer_value')->nullable();
+            $table->double('float_value')->nullable();
+            $table->dateTime('datetime_value')->nullable();
+            $table->date('date_value')->nullable();
+            $table->json('json_value')->nullable();
+
+            $table->integer('entity_id')->unsigned();
+            $table->integer('attribute_id')->unsigned();
+
+            $table->foreign('attribute_id')->references('id')->on('attributes')->onDelete('cascade');
+            $table->unique(['entity_type', 'entity_id', 'attribute_id'], 'entity_type_attribute_value_index_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_values');
+    }
+}
