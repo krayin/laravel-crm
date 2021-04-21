7588c4baf1e551320ade11f35a31e19d4a54fa02
jitendra <jitendra@webkul.com>
2021-04-21 21:14:38 +0530

    All attribute type completed

---
+++ packages/Webkul/Admin/src/Resources/views/contacts/persons/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('admin::layouts.master')
+
+@section('page_title')
+    {{ __('admin::app.contacts.persons.edit-title') }}
+@stop
+
+@section('content-wrapper')
+    <div class="content full-page adjacent-center">
+        <div class="page-header">
+            <div class="page-title">
+                <h1>{{ __('admin::app.contacts.persons.edit-title') }}</h1>
+            </div>
+        </div>
+
+        <form method="POST" action="{{ route('admin.contacts.persons.update', $person->id) }}" @submit.prevent="onSubmit" enctype="multipart/form-data">
+
+            <div class="page-content">
+                <div class="form-container">
+
+                    <div class="panel">
+                        <div class="panel-header">
+                            <button type="submit" class="btn btn-md btn-primary">
+                                {{ __('admin::app.contacts.persons.save-btn-title') }}
+                            </button>
+
+                            <a href="">{{ __('admin::app.contacts.persons.back') }}</a>
+                        </div>
+        
+                        <div class="panel-body">
+                            @csrf()
+                            <input name="_method" type="hidden" value="PUT">
+                
+                            @include('admin::common.custom-attribute-controls', [
+                                'customAttributes' => app('Webkul\Attribute\Repositories\AttributeRepository')->findWhere([
+                                    'entity_type' => 'persons',
+                                ]),
+                                'entity'           => $person,
+                            ])
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </form>
+    </div>
+@stop
