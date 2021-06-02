e563dcca44c23994f400770850c77055a763f6b8
Shubham Mehrotra <shubh0896m@gmail.com>
2021-06-02 10:32:55 +0530

    remove notificatio configuration

--- packages/Webkul/Admin/src/Config/core_config.php
+++ packages/Webkul/Admin/src/Config/core_config.php
@@ -29,24 +29,5 @@ return [
                 ],
             ],
         ],
-    ], [
-        'key'  => 'emails',
-        'name' => 'admin::app.configuration.emails.email',
-        'sort' => 1,
-    ], [
-        'key'  => 'emails.general',
-        'name' => 'admin::app.configuration.emails.notification_label',
-        'sort' => 1,
-    ], [
-        'key'    => 'emails.general.notifications',
-        'name'   => 'admin::app.configuration.emails.notification_label',
-        'sort'   => 1,
-        'fields' => [
-            [
-                'name'  => 'emails.general.notifications.new_lead',
-                'title' => 'admin::app.configuration.emails.new_lead',
-                'type'  => 'boolean',
-            ],
-        ],
     ],
 ];
