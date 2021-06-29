7d7efc686fc3d6d969073caa35f84f0c29479232
Shubham Mehrotra <shubh0896m@gmail.com>
2021-06-29 16:11:50 +0530

    concord file added

---
+++ packages/Webkul/Core/src/Config/concord.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'modules' => [
+        \Webkul\Admin\Providers\ModuleServiceProvider::class,
+        \Webkul\Attribute\Providers\ModuleServiceProvider::class,
+        \Webkul\Contact\Providers\ModuleServiceProvider::class,
+        \Webkul\Core\Providers\ModuleServiceProvider::class,
+        \Webkul\Email\Providers\ModuleServiceProvider::class,
+        \Webkul\Lead\Providers\ModuleServiceProvider::class,
+        \Webkul\Product\Providers\ModuleServiceProvider::class,
+        \Webkul\Tag\Providers\ModuleServiceProvider::class,
+        \Webkul\UI\Providers\ModuleServiceProvider::class,
+        \Webkul\User\Providers\ModuleServiceProvider::class,
+    ],
+    'register_route_models' => true
+];
