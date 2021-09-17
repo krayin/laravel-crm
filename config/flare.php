7c66a7f1a4b48f0dc965ee0bc081f323dd6347eb
jitendra <jitendra@webkul.com>
2021-09-17 19:49:27 +0530

    Issue #230 fixed

--- config/flare.php
+++ config/flare.php
@@ -34,6 +34,7 @@ return [
         'grouping_type' => null,
         'report_logs' => true,
         'maximum_number_of_collected_logs' => 200,
+        'censor_request_body_fields' => ['password'],
     ],
 
     /*
@@ -41,10 +42,21 @@ return [
     | Reporting Log statements
     |--------------------------------------------------------------------------
     |
-    | If this setting is `false` log statements won't be send as events to Flare,
+    | If this setting is `false` log statements won't be sent as events to Flare,
     | no matter which error level you specified in the Flare log channel.
     |
     */
 
     'send_logs_as_events' => true,
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Censor request body fields
+    |--------------------------------------------------------------------------
+    |
+    | These fields will be censored from your request when sent to Flare. 
+    |
+    */
+    
+    'censor_request_body_fields' => ['password'],
 ];
--- packages/Webkul/Core/src/Console/Commands/Install.php
+++ packages/Webkul/Core/src/Console/Commands/Install.php
@@ -55,7 +55,7 @@ class Install extends Command
 
         // running `php artisan vendor:publish --all`
         $this->warn('Step: Publishing assets and configurations...');
-        $result = $this->call('vendor:publish', ['--all']);
+        $result = $this->call('vendor:publish', ['--all' => true, '--force' => true]);
         $this->info($result);
 
         // running `php artisan storage:link`
@@ -118,7 +118,7 @@ class Install extends Command
             $input_admin_path = $this->ask('Please Enter the Admin URL : ');
             $this->envUpdate('APP_ADMIN_PATH=', $input_admin_path ?: $default_admin_path);
 
-            $locale = $this->choice('Please select the default locale or press enter to continue', ['ar', 'en', 'fa', 'nl', 'pt_BR'], 1);
+            $locale = $this->choice('Please select the default available locale or press enter to continue', ['en'], 0);
             $this->envUpdate('APP_LOCALE=', $locale);
 
             $TimeZones = timezone_identifiers_list();
