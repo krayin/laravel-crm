1b3ed9eb9958007caef4730a5efba96f08f74d92
Prateek Srivastava <prateek.srivastava781@webkul.com>
2021-10-11 17:57:48 +0530

    Toggle Validations Addd

--- packages/Webkul/Admin/src/Resources/views/settings/attributes/create.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/attributes/create.blade.php
@@ -172,7 +172,7 @@
                 <div class="form-group">
                     <label>{{ __('admin::app.settings.attributes.options-type') }}</label>
 
-                    <select class="control" name="option_type" v-model="optionType">
+                    <select class="control" name="option_type" v-model="optionType" @change="toggleInputValidation">
                         <option value="lookup">
                             {{ __('admin::app.settings.attributes.lookup') }}
                         </option>
@@ -296,6 +296,14 @@
 
                     Vue.delete(this.optionRows, index);
                 },
+
+                toggleInputValidation: function () {
+                    if (this.optionType === 'options') {
+                        $('#validation').prop('disabled', true);
+                    } else {
+                        $('#validation').prop('disabled', false);
+                    }
+                }
             },
         });
     </script>
