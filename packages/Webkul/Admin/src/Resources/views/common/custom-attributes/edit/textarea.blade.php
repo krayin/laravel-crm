167d438b1c2f8f03aad820d6b61794670de363fb
Devansh <devansh.bawari419@webkul.com>
2021-11-22 18:38:05 +0530

    V Pre Added

--- packages/Webkul/Admin/src/Resources/views/common/custom-attributes/edit/textarea.blade.php
+++ packages/Webkul/Admin/src/Resources/views/common/custom-attributes/edit/textarea.blade.php
@@ -4,4 +4,4 @@
     id="{{ $attribute->code }}"
     v-validate="'{{$validations}}'"
     data-vv-as="&quot;{{ $attribute->name }}&quot;"
->{{ old($attribute->code) ?: $value}}</textarea>
+    v-pre>{{ old($attribute->code) ?: $value}}</textarea>
