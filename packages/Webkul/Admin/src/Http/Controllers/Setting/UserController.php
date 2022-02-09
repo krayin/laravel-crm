1d6e8c7df5ed8c69db83873d5331c2b281649467
Sanjay <sanjay.bhatt371@webkul.com>
2022-02-09 12:43:39 +0530

    Fixed exception on user-create

--- packages/Webkul/Admin/src/Http/Controllers/Setting/UserController.php
+++ packages/Webkul/Admin/src/Http/Controllers/Setting/UserController.php
@@ -110,7 +110,8 @@ class UserController extends Controller
         try {
             Mail::queue(new Create($admin));
         } catch (\Exception $e) {
-            report($e);
+            //report($e);
+            session()->flash('success', $e->getMessage());
         }
 
         Event::dispatch('settings.user.create.after', $admin);
