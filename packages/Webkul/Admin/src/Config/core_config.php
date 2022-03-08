70ee682ee50700b02d2f4cb4334e33f225c42b4f
Sanjay <sanjay.bhatt371@webkul.com>
2022-03-08 19:58:10 +0530

    Add-locale-option-UI-CREATED

--- packages/Webkul/Admin/src/Config/core_config.php
+++ packages/Webkul/Admin/src/Config/core_config.php
@@ -17,5 +17,34 @@ return [
                 'data_source'   => 'Webkul\Core\Core@locales'
             ],
         ],
+    ],[
+        'key'    => 'general.add_locale',
+        'name'   => 'Add Locale',
+        'sort'   => 1,
+        'validation'    => 'required',
+        'fields' => [
+            [
+                'name'          => 'Code',
+                'title'         => 'Code',
+                'type'          => 'text',
+            ],[
+                'name'          => 'Name',
+                'title'         => 'Name',
+                'type'          => 'text',
+            ],[
+                'name'          => 'Direction',
+                'title'         => 'Direction',
+                'type'          => 'select',
+                'options'       => [
+                    [
+                        'title' => 'RTL',
+                        'value' => 'rtl',
+                    ], [
+                        'title' => 'LTR',
+                        'value' => 'ltr',
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
