55a4fc8dc38093066c43b348707e1e43fffc66f3
Mariusz <mariuszmalek.dev@gmail.com>
2022-09-22 07:56:55 +0200

    add column to migration table

---
+++ packages/Webkul/Attribute/src/Database/Migrations/2022_09_22_112532_add_is_visible_in_attributes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('attributes', function (Blueprint $table) {
+            $table->boolean('is_visible')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attributes', function (Blueprint $table) {
+            $table->boolean('is_visible')->default(0);
+        });
+    }
+};
