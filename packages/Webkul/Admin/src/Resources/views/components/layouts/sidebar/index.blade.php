3f131736ed7fc8aaab3402648540ae74c066d90c
suraj kashyap <suraj.kashyap370@webkul.in>
2024-07-16 10:32:13 +0530

    Sidebar update.

--- packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
@@ -1,5 +1,5 @@
 <div 
-    class="fixed top-14 z-[1000] h-full w-[270px] bg-white pt-4 shadow-[0px_8px_10px_0px_rgba(0,_0,_0,_0.2)] transition-all duration-300 group-[.sidebar-collapsed]/container:w-[70px] dark:bg-gray-900 max-lg:hidden"
+    class="fixed top-14 z-[1000] h-full w-[270px] bg-white pt-4 shadow-[0px_8px_10px_0px_rgba(0,_0,_0,_0.2)] group-[.sidebar-collapsed]/container:w-[70px] rounded-r-3xl dark:bg-gray-900 max-lg:hidden"
     @mouseover="handleMouseOver"
     @mouseleave="handleMouseLeave"
 >
@@ -23,15 +23,28 @@
                     </a>
 
                     @if ($menuItem->haveChildren())
-                        <div class="fixed z-[100] hidden min-w-[180px] overflow-hidden rounded-none rounded-b-lg border border-gray-300 bg-white !p-0 pb-2 group-hover/item:!grid group-[.sidebar-collapsed]/container:fixed group-[.sidebar-collapsed]/container:!hidden group-[.sidebar-collapsed]/container:rounded-none group-[.sidebar-collapsed]/container:border group-[.sidebar-collapsed]/container:border-gray-300 group-[.sidebar-collapsed]/container:bg-white group-[.sidebar-collapsed]/container:p-[0] dark:border-gray-800 dark:bg-gray-900 group-[.sidebar-collapsed]/container:dark:border-gray-800 dark:group-[.sidebar-collapsed]/container:bg-gray-900 ltr:left-[270px] ltr:rounded-r-lg ltr:pl-10 ltr:shadow-[34px_10px_14px_rgba(0,0,0,0.01),19px_6px_12px_rgba(0,0,0,0.03),9px_3px_9px_rgba(0,0,0,0.04),2px_1px_5px_rgba(0,0,0,0.05),0px_0px_0px_rgba(0,0,0,0.05)] group-[.sidebar-collapsed]/container:ltr:!left-[70px] group-[.sidebar-collapsed]/container:ltr:rounded-r-lg group-[.sidebar-collapsed]/container:ltr:shadow-[34px_10px_14px_rgba(0,0,0,0.01),19px_6px_12px_rgba(0,0,0,0.03),9px_3px_9px_rgba(0,0,0,0.04),2px_1px_5px_rgba(0,0,0,0.05),0px_0px_0px_rgba(0,0,0,0.05)] rtl:right-[270px] rtl:rounded-l-lg rtl:pr-10 rtl:shadow-[-34px_10px_14px_rgba(0,0,0,0.01),-19px_6px_12px_rgba(0,0,0,0.03),-9px_3px_9px_rgba(0,0,0,0.04),-2px_1px_5px_rgba(0,0,0,0.05),-0px_0px_0px_rgba(0,0,0,0.05)] group-[.sidebar-collapsed]/container:rtl:!right-[70px] group-[.sidebar-collapsed]/container:rtl:rounded-l-lg group-[.sidebar-collapsed]/container:rtl:shadow-[-34px_10px_14px_rgba(0,0,0,0.01),-19px_6px_12px_rgba(0,0,0,0.03),-9px_3px_9px_rgba(0,0,0,0.04),-2px_1px_5px_rgba(0,0,0,0.05),-0px_0px_0px_rgba(0,0,0,0.05)]">
-                            @foreach ($menuItem->getChildren() as $subMenuItem)
-                                <a
-                                    href="{{ $subMenuItem->getUrl() }}"
-                                    class="text-sm text-{{ $subMenuItem->isActive() ? 'blue':'gray' }}-600 dark:text-{{ $subMenuItem->isActive() ? 'blue':'gray' }}-300 whitespace-nowrap py-1 group-[.sidebar-collapsed]/container:px-5 group-[.sidebar-collapsed]/container:py-2.5 px-5 py-2.5 hover:text-blue-600 dark:hover:bg-gray-950"
-                                >
-                                    {{ $subMenuItem->getName() }}
-                                </a>
-                            @endforeach
+                        <div class="h-[calc(100vh-60px)] fixed z-[100] hidden min-w-[180px] overflow-hidden rounded-none rounded-b-lg border border-gray-300 bg-white !p-0 pb-2 group-hover/item:!flex group-hover/item:!flex-col group-hover/item:!gap-2 group-[.sidebar-collapsed]/container:fixed group-[.sidebar-collapsed]/container:!hidden group-[.sidebar-collapsed]/container:rounded-none group-[.sidebar-collapsed]/container:border group-[.sidebar-collapsed]/container:border-gray-300 group-[.sidebar-collapsed]/container:bg-white group-[.sidebar-collapsed]/container:p-[0] dark:border-gray-800 dark:bg-gray-900 group-[.sidebar-collapsed]/container:dark:border-gray-800 dark:group-[.sidebar-collapsed]/container:bg-gray-900 ltr:left-[270px] ltr:rounded-r-lg ltr:pl-10 ltr:shadow-[34px_10px_14px_rgba(0,0,0,0.01),19px_6px_12px_rgba(0,0,0,0.03),9px_3px_9px_rgba(0,0,0,0.04),2px_1px_5px_rgba(0,0,0,0.05),0px_0px_0px_rgba(0,0,0,0.05)] group-[.sidebar-collapsed]/container:ltr:!left-[70px] group-[.sidebar-collapsed]/container:ltr:rounded-r-lg group-[.sidebar-collapsed]/container:ltr:shadow-[34px_10px_14px_rgba(0,0,0,0.01),19px_6px_12px_rgba(0,0,0,0.03),9px_3px_9px_rgba(0,0,0,0.04),2px_1px_5px_rgba(0,0,0,0.05),0px_0px_0px_rgba(0,0,0,0.05)] rtl:right-[270px] rtl:rounded-l-lg rtl:pr-10 rtl:shadow-[-34px_10px_14px_rgba(0,0,0,0.01),-19px_6px_12px_rgba(0,0,0,0.03),-9px_3px_9px_rgba(0,0,0,0.04),-2px_1px_5px_rgba(0,0,0,0.05),-0px_0px_0px_rgba(0,0,0,0.05)] group-[.sidebar-collapsed]/container:rtl:!right-[70px] group-[.sidebar-collapsed]/container:rtl:rounded-l-lg group-[.sidebar-collapsed]/container:rtl:shadow-[-34px_10px_14px_rgba(0,0,0,0.01),-19px_6px_12px_rgba(0,0,0,0.03),-9px_3px_9px_rgba(0,0,0,0.04),-2px_1px_5px_rgba(0,0,0,0.05),-0px_0px_0px_rgba(0,0,0,0.05)]">
+
+                            <div 
+                                class="fixed top-14 z-[1000] h-full w-[270px] bg-white pt-4 shadow-[0px_8px_10px_0px_rgba(0,_0,_0,_0.2)] group-[.sidebar-collapsed]/container:w-[70px] rounded-r-3xl dark:bg-gray-900 max-lg:hidden"
+                            >
+                                <div class="journal-scroll h-[calc(100vh-100px)] overflow-auto group-[.sidebar-collapsed]/container:overflow-visible">
+                                    <nav class="grid w-full gap-2">
+                                        @foreach ($menuItem->getChildren() as $subMenuItem)
+                                            <div class="px-4 group/item {{ $menuItem->isActive() ? 'active' : 'inactive' }}">
+                                                <a
+                                                    href="{{ $subMenuItem->getUrl() }}"
+                                                    class="flex gap-2.5 p-1.5 items-center cursor-pointer hover:rounded-lg {{ $subMenuItem->isActive() == 'active' ? 'bg-blue-600 rounded-lg' : ' hover:bg-gray-100 hover:dark:bg-gray-950' }} peer"
+                                                >
+                                                    <p class="text-gray-600 dark:text-gray-300 font-semibold whitespace-nowrap group-[.sidebar-collapsed]/container:hidden {{ $subMenuItem->isActive() ? 'text-white' : ''}}">
+                                                        {{ $subMenuItem->getName() }}
+                                                    </p>
+                                                </a>
+                                            </div>
+                                        @endforeach
+                                    </nav>
+                                </div>
+                            </div>
                         </div>
                     @endif
                 </div>
