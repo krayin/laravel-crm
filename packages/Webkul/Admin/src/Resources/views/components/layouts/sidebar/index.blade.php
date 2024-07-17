bfe2b507292c6dc7c56ca7e2f7a3813f9a5b5e27
Shivendra <shivendra.gupta423@webkul.in>
2024-07-17 11:08:28 +0530

    added icon and fix width

--- packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
@@ -1,10 +1,10 @@
 <div 
-    class="fixed top-14 z-[1000] h-full w-[270px] bg-white pt-4 group-[.sidebar-collapsed]/container:w-[70px] dark:bg-gray-900 max-lg:hidden"
+    class="duration-80 fixed top-14 z-[1000] h-full w-[190px] bg-white pt-4 transition-all group-[.sidebar-collapsed]/container:w-[70px] dark:bg-gray-900 max-lg:hidden"
     @mouseover="handleMouseOver"
     @mouseleave="handleMouseLeave"
 >
     <div class="journal-scroll h-[calc(100vh-100px)] overflow-auto group-[.sidebar-collapsed]/container:overflow-visible">
-        <nav class="grid w-full gap-2">
+        <nav class="sidebar-rounded grid w-full gap-2">
             <!-- Navigation Menu -->
             @foreach (menu()->getItems('admin') as $menuItem)
                 <div class="px-4 group/item {{ $menuItem->isActive() ? 'active' : 'inactive' }}">
@@ -14,14 +14,19 @@
                     >
                         <span class="{{ $menuItem->getIcon() }} text-2xl {{ $menuItem->isActive() ? 'text-white' : ''}}"></span>
                         
-                        <p class="text-gray-600 dark:text-gray-300 font-medium whitespace-nowrap group-[.sidebar-collapsed]/container:hidden {{ $menuItem->isActive() ? 'text-white' : ''}}">
-                            {{ $menuItem->getName() }}
-                        </p>
+                        <div class="flex-1 flex justify-between items-center text-gray-600 dark:text-gray-300 font-medium whitespace-nowrap group-[.sidebar-collapsed]/container:hidden {{ $menuItem->isActive() ? 'text-white' : ''}} group">
+                            <p>{{ $menuItem->getName() }}</p>
+                        
+                            @if ($menuItem->haveChildren())
+                                <i class="icon-arrow-left invisible text-2xl group-hover:visible {{ $menuItem->isActive() ? 'text-white' : ''}}"></i>
+                            @endif
+                        </div>
                     </a>
 
+                    <!-- Submenu -->
                     @if ($menuItem->haveChildren())
-                        <div class="fixed z-[100] hidden h-[calc(100vh-60px)] min-w-[180px] overflow-hidden rounded-none rounded-b-lg border border-gray-300 bg-white !p-0 pb-2 group-hover/item:!flex group-hover/item:!flex-col group-hover/item:!gap-2 dark:border-gray-800 dark:bg-gray-900 ltr:left-[270px] ltr:rounded-r-lg ltr:pl-10 rtl:right-[270px] rtl:rounded-l-lg rtl:pr-10">
-                            <div class="fixed top-14 z-[1000] h-full w-[200px] bg-white pt-4 shadow-[0px_8px_10px_0px_rgba(0,_0,_0,_0.2)] dark:bg-gray-900 max-lg:hidden">
+                        <div class="fixed z-[100] hidden h-[calc(100vh-60px)] min-w-[150px] overflow-hidden rounded-none rounded-b-lg bg-white !p-0 pb-2 group-hover/item:!flex group-hover/item:!flex-col group-hover/item:!gap-2 dark:bg-gray-900 ltr:left-[190px] ltr:rounded-r-lg ltr:pl-10 rtl:right-[190px] rtl:rounded-l-lg rtl:pr-10">
+                            <div class="sidebar-rounded fixed top-14 z-[1000] h-full w-[150px] bg-white pt-4 dark:bg-gray-900 max-lg:hidden">
                                 <div class="journal-scroll h-[calc(100vh-100px)] overflow-auto">
                                     <nav class="grid w-full gap-2">
                                         @foreach ($menuItem->getChildren() as $subMenuItem)
