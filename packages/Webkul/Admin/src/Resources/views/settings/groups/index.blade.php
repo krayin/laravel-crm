f7355af9f4fa14c5bb29d68550d04c9003ab1da3
suraj kashyap <suraj.kashyap370@webkul.in>
2024-07-18 18:47:30 +0530

    Fix minor issues.

--- packages/Webkul/Admin/src/Resources/views/settings/groups/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/groups/index.blade.php
@@ -103,13 +103,13 @@
                             class="row grid items-center gap-2.5 border-b px-4 py-4 text-gray-600 transition-all hover:bg-gray-50 dark:border-gray-800 dark:text-gray-300 dark:hover:bg-gray-950"
                             :style="`grid-template-columns: repeat(${gridsCount}, minmax(0, 1fr))`"
                         >
-                            <!-- Currency ID -->
+                            <!-- Group ID -->
                             <p>@{{ record.id }}</p>
         
-                            <!-- Currency Name -->
+                            <!-- Group Name -->
                             <p>@{{ record.name }}</p>
         
-                            <!-- Currency Code -->
+                            <!-- Group Code -->
                             <p>@{{ record.description }}</p>
         
                             <!-- Actions -->
