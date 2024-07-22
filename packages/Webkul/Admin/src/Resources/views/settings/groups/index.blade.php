2db7d2426de405881a98aa1a28bac61b7e90d959
Shivendra <shivendra.gupta423@webkul.in>
2024-07-22 11:26:57 +0530

    added breadcrums

--- packages/Webkul/Admin/src/Resources/views/settings/groups/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/groups/index.blade.php
@@ -4,20 +4,13 @@
     </x-slot>
 
     <v-group-settings>
-        <div class="flex items-center justify-between rounded-lg border border-gray-200 bg-white py-2 pl-2 pr-4 text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300">
-            <div class="flex flex-col">
+        <div class="flex items-center justify-between rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300">
+            <div class="flex flex-col gap-2">
                 <div class="flex cursor-pointer items-center">
-                    <i class="icon-left-arrow text-2xl text-gray-800"></i>
-    
-                    <a
-                        href="{{ route('admin.settings.groups.index') }}"
-                        class="text-xs text-gray-800 dark:text-gray-300"
-                    >
-                        Settings
-                    </a>
+                    <x-admin::breadcrumbs name="settings.groups" />
                 </div>
     
-                <div class="pl-3 text-xl font-normal dark:text-gray-300">
+                <div class="text-xl font-bold dark:text-gray-300">
                     @lang('admin::app.settings.groups.index.title')
                 </div>
             </div>
@@ -44,24 +37,17 @@
             type="text/x-template"
             id="group-settings-template"
         >
-            <div class="flex items-center justify-between rounded-lg border border-gray-200 bg-white py-2 pl-2 pr-4 text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300">
-                <div class="flex flex-col">
+            <div class="flex items-center justify-between rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300">
+                <div class="flex flex-col gap-2">
                     <div class="flex cursor-pointer items-center">
-                        <i class="icon-left-arrow text-2xl text-gray-800"></i>
-        
-                        <a
-                            href="{{ route('admin.settings.groups.index') }}"
-                            class="text-xs text-gray-800 dark:text-gray-300"
-                        >
-                            Settings
-                        </a>
+                        <x-admin::breadcrumbs name="settings.groups" />
                     </div>
         
-                    <div class="pl-3 text-xl font-normal dark:text-gray-300">
+                    <div class="text-xl font-bold dark:text-gray-300">
                         @lang('admin::app.settings.groups.index.title')
                     </div>
                 </div>
-        
+
                 <div class="flex items-center gap-x-2.5">
                     <!-- Create button for person -->
                     <div class="flex items-center gap-x-2.5">
