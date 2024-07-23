2e4322ca726e142dc5a65d42c81d7932d7270de8
Shivendra <shivendra.gupta423@webkul.in>
2024-07-23 17:50:37 +0530

    Refactor cantacts and products routes

--- packages/Webkul/Admin/src/Routes/settings-routes.php
+++ packages/Webkul/Admin/src/Routes/settings-routes.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Webkul\Admin\Http\Controllers\Settings\GroupController;
 use Webkul\Admin\Http\Controllers\Settings\RoleController;
+use Webkul\Admin\Http\Controllers\Settings\SourceController;
 use Webkul\Admin\Http\Controllers\Settings\TypeController;
 
 /**
@@ -15,8 +16,6 @@ Route::group(['middleware' => ['admin_locale'], 'prefix' => config('app.admin_pa
          */
         Route::controller(GroupController::class)->prefix('groups')->group(function () {
             Route::get('', 'index')->name('admin.settings.groups.index');
-
-            Route::get('create', 'create')->name('admin.settings.groups.create');
 
             Route::post('create', 'store')->name('admin.settings.groups.store');
 
@@ -57,6 +56,21 @@ Route::group(['middleware' => ['admin_locale'], 'prefix' => config('app.admin_pa
             Route::put('edit/{id}', 'update')->name('admin.settings.roles.update');
 
             Route::delete('{id}', 'destroy')->name('admin.settings.roles.delete');
+        });
+
+        /**
+         * Lead Sources routes.
+         */
+        Route::controller(SourceController::class)->prefix('sources')->group(function () {
+            Route::get('', 'index')->name('admin.settings.sources.index');
+
+            Route::post('create', 'store')->name('admin.settings.sources.store');
+
+            Route::get('edit/{id?}', 'edit')->name('admin.settings.sources.edit');
+
+            Route::put('edit/{id}', 'update')->name('admin.settings.sources.update');
+
+            Route::delete('{id}', 'destroy')->name('admin.settings.sources.delete');
         });
     });
 });
--- packages/Webkul/Admin/src/Routes/web.php
+++ packages/Webkul/Admin/src/Routes/web.php
@@ -4,3 +4,13 @@
  * Settings routes.
  */
 require 'settings-routes.php';
+
+/**
+ * Products routes.
+ */
+require 'products-routes.php';
+
+/**
+ * Contacts routes.
+ */
+require 'contacts-routes.php';
