ac13991bf8eead35fa3995f8318c41b11fcf1cef
Shivendra <shivendra.gupta423@webkul.in>
2024-07-30 12:33:34 +0530

    removed unusable shimmer files

---
+++ packages/Webkul/Admin/src/Resources/views/components/shimmer/common/address.blade.php
@@ -0,0 +1,15 @@
+<div class="flex gap-4">
+    <div class="w-full">
+        <div class="shimmer w-50% h-56"></div>
+    </div>
+
+    <div class="grid w-full gap-3">
+        <div class="shimmer w-50% h-10"></div>
+
+        <div class="shimmer w-50% h-10"></div>
+
+        <div class="shimmer w-50% h-10"></div>
+        
+        <div class="shimmer w-50% h-10"></div>
+    </div>
+</div>
