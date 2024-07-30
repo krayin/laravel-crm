b38a20bb9460a7f58854d9a4fa4e5217b4d9e7f2
suraj kashyap <suraj.kashyap370@webkul.in>
2024-07-30 16:13:42 +0530

    Revert some changes.

--- packages/Webkul/Admin/src/Resources/views/dashboard/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/dashboard/index.blade.php
@@ -1,20 +1,3 @@
 <x-admin::layouts>
 <p>Dashboard</p>
-<x-admin::form.control-group>
-    <x-admin::form.control-group.label class="required">
-        @lang('Price')
-    </x-admin::form.control-group.label>
-
-    <x-admin::form.control-group.control
-        type="inline"
-        id="price"
-        name="price"
-        value="$2502"
-        position="right"
-        @onChange="onPriceChange"
-        @onCancelled="onPriceEditingCancelled"
-    />
-
-    <x-admin::form.control-group.error control-name="price" />
-</x-admin::form.control-group>
 </x-admin::layouts>
