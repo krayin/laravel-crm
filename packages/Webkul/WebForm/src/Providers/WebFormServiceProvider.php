3514caca979561b6f215355dfb7674981ad8806e
suraj kashyap <suraj.kashyap370@webkul.in>
2024-07-30 18:51:59 +0530

    webform.

--- packages/Webkul/WebForm/src/Providers/WebFormServiceProvider.php
+++ packages/Webkul/WebForm/src/Providers/WebFormServiceProvider.php
@@ -2,58 +2,40 @@
 
 namespace Webkul\WebForm\Providers;
 
-use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class WebFormServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap services.
-     *
-     * @return void
      */
-    public function boot(Router $router)
+    public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__.'/../Http/routes.php');
 
         $this->loadTranslationsFrom(__DIR__.'/../Resources/lang', 'web_form');
 
-        $this->publishes([
-            __DIR__.'/../../publishable/assets' => public_path('vendor/webkul/web-form/assets'),
-        ], 'public');
-
         $this->loadViewsFrom(__DIR__.'/../Resources/views', 'web_form');
 
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
-
-        Event::listen('admin.layout.head', function ($viewRenderEventManager) {
-            $viewRenderEventManager->addTemplate('web_form::layouts.style');
-        });
 
         $this->app->register(ModuleServiceProvider::class);
     }
 
     /**
      * Register services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->registerConfig();
     }
 
     /**
      * Register package config.
-     *
-     * @return void
      */
-    protected function registerConfig()
+    protected function registerConfig(): void
     {
-        $this->mergeConfigFrom(
-            dirname(__DIR__).'/Config/menu.php', 'menu.admin'
-        );
+        $this->mergeConfigFrom(dirname(__DIR__).'/Config/menu.php', 'menu.admin');
 
         $this->mergeConfigFrom(dirname(__DIR__).'/Config/acl.php', 'acl');
     }
