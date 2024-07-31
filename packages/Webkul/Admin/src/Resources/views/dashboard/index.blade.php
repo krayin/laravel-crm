e69423070289043f8343233dec9c3f1f5849dd08
suraj kashyap <suraj.kashyap370@webkul.in>
2024-07-31 20:17:55 +0530

    revert some chages.

--- packages/Webkul/Admin/src/Resources/views/dashboard/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/dashboard/index.blade.php
@@ -1,8 +1,3 @@
 <x-admin::layouts>
 <p>Dashboard</p>
-    <x-admin::lookup 
-        end-point="{{ route('admin.settings.attributes.lookup') }}/persons"
-        name="organizations"
-        placeholder="Search Organizations"
-    />
 </x-admin::layouts>
