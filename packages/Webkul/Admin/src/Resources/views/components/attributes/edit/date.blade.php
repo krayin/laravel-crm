623932691d0e8c63aca2db99540821158a66337e
Shivendra <shivendra.gupta423@webkul.in>
2024-08-06 11:42:42 +0530

    update component

--- packages/Webkul/Admin/src/Resources/views/components/attributes/edit/date.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/attributes/edit/date.blade.php
@@ -1,9 +1,8 @@
-<x-admin::flat-picker.date ::allow-input="false">
-    <input
-        type="text"
-        name="{{ $attribute->code }}"
-        value="{{ old($attribute->code) ?? $value }}"
-        class="flex min-h-[39px] w-full rounded-md border px-3 py-2 text-sm text-gray-600 transition-all hover:border-gray-400 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-gray-400"
-        placeholder="{{ $attribute->name }}"
-    />
-</x-admin::flat-picker.date>
+<x-admin::form.control-group.control
+    type="date"
+    :id="$attribute->code"
+    :name="$attribute->code"
+    :value="old($attribute->code) ?? $value"
+    :rules="$validations"
+    :label="$attribute->name"
+/>
