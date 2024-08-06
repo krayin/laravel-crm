27dfc8a2abfb7badd050bd8916c4ebeeb38f13ef
Jitendra <jitendra@webkul.in>
2024-08-06 18:14:18 +0530

    Changes to attribute inline edit

--- packages/Webkul/Admin/src/Resources/views/components/attributes/view/price.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/attributes/view/price.blade.php
@@ -1,1 +1,14 @@
-{{ ! is_null($value) ? core()->formatBasePrice($value) : __('admin::app.common.not-available') }}
+{{-- {{ ! is_null($value) ? core()->formatBasePrice($value) : __('admin::app.common.not-available') }} --}}
+
+<x-admin::form.control-group class="!mb-0">
+    <x-admin::form.control-group.control
+        type="inline"
+        :name="$attribute->name"
+        :value="$value"
+        rules="required|decimal:4"
+        ::errors="errors"
+        :label="trans('admin::app.quotes.create.discount-amount')"
+        :placeholder="trans('admin::app.quotes.create.discount-amount')"
+        @on-change="(value) => product.discount_amount = value"
+    />
+</x-admin::form.control-group>
