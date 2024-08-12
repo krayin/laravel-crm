55bad16ae4fb98ffb369b4ddb8c969d61a3a0980
suraj kashyap <suraj.kashyap370@webkul.in>
2024-08-12 11:35:48 +0530

    use svg blade component.

--- packages/Webkul/Admin/src/Resources/views/mail/view.blade.php
+++ packages/Webkul/Admin/src/Resources/views/mail/view.blade.php
@@ -441,27 +441,7 @@
                             </template>
 
                             <template v-else>
-                                <svg
-                                    class="h-5 w-5 animate-spin"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    aria-hidden="true"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <circle
-                                        class="opacity-25"
-                                        cx="12"
-                                        cy="12"
-                                        r="10"
-                                        stroke="currentColor"
-                                        stroke-width="4"
-                                    ></circle>
-                                    <path
-                                        class="opacity-75"
-                                        fill="currentColor"
-                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                    ></path>
-                                </svg>
+                                <x-admin::spinner />
                             </template>
 
                             <a
@@ -529,27 +509,7 @@
                                             class="relative"
                                             v-if="isSearching"
                                         >
-                                            <svg
-                                                class="h-5 w-5 animate-spin"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                aria-hidden="true"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <circle
-                                                    class="opacity-25"
-                                                    cx="12"
-                                                    cy="12"
-                                                    r="10"
-                                                    stroke="currentColor"
-                                                    stroke-width="4"
-                                                ></circle>
-                                                <path
-                                                    class="opacity-75"
-                                                    fill="currentColor"
-                                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                                ></path>
-                                            </svg>
+                                            <x-admin::spinner />
                                         </div>
                             
                                         <!-- Search Icon -->
@@ -590,7 +550,7 @@
 
                             <!-- Add New Contact Button -->
                             <div
-                                class="flex items-center gap-2 p-2 border-t border-gray-200 cursor-pointer text-blue-600 transition-colors hover:bg-gray-100"
+                                class="flex items-center gap-2 p-2 border-t border-gray-200 cursor-pointer text-blue-600 transition-colors"
                                 @click="toggleContactModal"
                             >
                                 <span>+ @lang('admin::app.mail.view.add-new-contact')</span>
@@ -646,27 +606,7 @@
                             </template>
 
                             <template v-else>
-                                <svg
-                                    class="h-5 w-5 animate-spin"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    aria-hidden="true"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <circle
-                                        class="opacity-25"
-                                        cx="12"
-                                        cy="12"
-                                        r="10"
-                                        stroke="currentColor"
-                                        stroke-width="4"
-                                    ></circle>
-                                    <path
-                                        class="opacity-75"
-                                        fill="currentColor"
-                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                    ></path>
-                                </svg>
+                                <x-admin::spinner />
                             </template>
 
                             <a
@@ -735,27 +675,7 @@
                                             class="relative"
                                             v-if="isSearching"
                                         >
-                                            <svg
-                                                class="h-5 w-5 animate-spin"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                aria-hidden="true"
-                                                viewBox="0 0 24 24"
-                                            >
-                                                <circle
-                                                    class="opacity-25"
-                                                    cx="12"
-                                                    cy="12"
-                                                    r="10"
-                                                    stroke="currentColor"
-                                                    stroke-width="4"
-                                                ></circle>
-                                                <path
-                                                    class="opacity-75"
-                                                    fill="currentColor"
-                                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                                ></path>
-                                            </svg>
+                                            <x-admin::spinner />
                                         </div>
                             
                                         <!-- Search Icon -->
@@ -789,7 +709,7 @@
 
                             <!-- Add New Lead Button -->
                             <div
-                                class="flex items-center gap-2 p-2 border-t border-gray-200 cursor-pointer text-blue-600 transition-colors hover:bg-gray-100"
+                                class="flex items-center gap-2 p-2 border-t border-gray-200 cursor-pointer text-blue-600 transition-colors"
                                 @click="toggleLeadModal"
                             >
                                 <span>+ @lang('admin::app.mail.view.add-new-lead')</span>
