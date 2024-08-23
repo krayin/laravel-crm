f1deca97cdc0726e998faf0bcddce2f66e10d938
suraj kashyap <suraj.kashyap370@webkul.in>
2024-08-23 16:27:41 +0530

    fix datagrid shimmer.

--- packages/Webkul/Admin/src/Resources/views/components/shimmer/datagrid/toolbar.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/shimmer/datagrid/toolbar.blade.php
@@ -1,7 +1,7 @@
 <div class="mt-2 flex items-center justify-between gap-4 h-[58px] pr-4 pl-2 border border-b-0 rounded-t-lg bg-white border-gray-200 max-md:flex-wrap dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300">
     <!-- Left Toolbar -->
     <div class="flex gap-x-1.5 w-full items-center">
-        <div class="shimmer w-[302px] h-[38px] rounded-lg"></div>
+        <div class="shimmer w-[264px] h-[38px] rounded-lg"></div>
     
         <div class="shimmer w-[62px] h-[38px] rounded-lg"></div>
     </div>
