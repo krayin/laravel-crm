f1a861e495a24dc1d704bb86b672c7f5e6b98830
suraj kashyap <suraj.kashyap370@webkul.in>
2024-08-26 10:18:08 +0530

    fix validation into the address component.

--- packages/Webkul/Admin/src/Resources/lang/en/app.php
+++ packages/Webkul/Admin/src/Resources/lang/en/app.php
@@ -1615,6 +1615,8 @@ return [
             'home'           => 'Home',
             'add-more'       => 'Add More',
             'select'         => 'Select',
+            'country'        => 'Country',
+            'address'        => 'Address',
         ],
     ],
 
