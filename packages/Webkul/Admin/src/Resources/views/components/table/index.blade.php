8cf868c12f4b4a1668cf5d29847cb745870f547a
suraj kashyap <suraj.kashyap370@webkul.in>
2024-08-27 12:10:23 +0530

    minor changes.

--- packages/Webkul/Admin/src/Resources/views/components/table/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/table/index.blade.php
@@ -1,3 +1,3 @@
-<table {{ $attributes->merge(['class' => 'w-full min-w-[800px] text-left text-sm border border-gray-200']) }}>
+<table {{ $attributes->merge(['class' => 'w-full min-w-[800px] text-left text-sm border border-gray-200 dark:border-gray-800']) }}>
     {{ $slot }}
 </table>
