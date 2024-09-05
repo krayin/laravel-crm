08406a0295059c3e193d67abe9e88c5fce584749
suraj kashyap <suraj.kashyap370@webkul.in>
2024-09-05 19:38:38 +0530

    Remove arbetary value.

--- packages/Webkul/Admin/src/Resources/views/components/form/control-group/controls/inline/date.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/form/control-group/controls/inline/date.blade.php
@@ -70,7 +70,7 @@
                     type="date"
                     ::id="name"
                     ::name="name"
-                    class="text-normal py-1 ltr:pr-[85px] rtl:pl-[85px]"
+                    class="text-normal py-1 ltr:pr-20 rtl:pl-20"
                     ::rules="rules"
                     ::label="label"
                     ::placeholder="placeholder"
