30a6f97e86f704ca0168bb1f1651f02e8dd01aa9
suraj kashyap <suraj.kashyap370@webkul.in>
2024-09-09 12:42:15 +0530

    fix the ui of index and create page.

--- packages/Webkul/Admin/src/Resources/views/settings/data-transfer/imports/create.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/data-transfer/imports/create.blade.php
@@ -13,27 +13,37 @@
         {!! view_render_event('bagisto.admin.settings.data_transfer.imports.create.create_form_controls.before') !!}
 
         <!-- Page Header -->
-        <div class="flex items-center justify-between">
-            <p class="text-xl font-bold text-gray-800 dark:text-white">
-                @lang('admin::app.settings.data-transfer.imports.create.title')
-            </p>
+        <div class="flex items-center justify-between rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300">
+            <div class="flex flex-col gap-2">
+                <div class="flex cursor-pointer items-center">
+                    {!! view_render_event('admin.settings.data_transfers.create.breadcrumbs.before') !!}
+
+                    <!-- Breadcrumbs -->
+                    <x-admin::breadcrumbs name="settings.data_transfers.create" />
+
+                    {!! view_render_event('admin.settings.data_transfers.create.breadcrumbs.after') !!}
+                </div>
+
+                <div class="text-xl font-bold dark:text-white">
+                    @lang('admin::app.settings.data-transfer.imports.create.title')
+                </div>
+            </div>
 
             <div class="flex items-center gap-x-2.5">
-                <!-- Back Button -->
-                <a
-                    href="{{ route('admin.settings.data_transfer.imports.index') }}"
-                    class="transparent-button hover:bg-gray-200 dark:text-white dark:hover:bg-gray-800"
-                >
-                    @lang('admin::app.settings.data-transfer.imports.create.back-btn')
-                </a>
+                <!-- Create button for person -->
+                <div class="flex items-center gap-x-2.5">
+                    {!! view_render_event('admin.settings.data_transfers.create.save_button.before') !!}
 
-                <!-- Save Button -->
-                <button
-                    type="submit"
-                    class="primary-button"
-                >
-                    @lang('admin::app.settings.data-transfer.imports.create.save-btn')
-                </button>
+                    <!-- Save Button -->
+                    <button
+                        type="submit"
+                        class="primary-button"
+                    >
+                        @lang('admin::app.settings.data-transfer.imports.create.save-btn')
+                    </button>
+
+                    {!! view_render_event('admin.settings.data_transfers.create.save_button.after') !!}
+                </div>
             </div>
         </div>
 
@@ -59,7 +69,7 @@
                             type="select"
                             name="type"
                             id="import-type"
-                            :value="old('type') ?? 'products'"
+                            :value="old('type') ?? 'persons'"
                             ref="importType"
                             rules="required"
                             :label="trans('admin::app.settings.data-transfer.imports.create.type')"
--- packages/Webkul/Admin/src/Resources/views/settings/data-transfer/imports/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/settings/data-transfer/imports/index.blade.php
@@ -3,20 +3,50 @@
         @lang('admin::app.settings.data-transfer.imports.index.title')
     </x-slot>
 
-    <div class="flex items-center justify-between gap-4 max-sm:flex-wrap">
-        <p class="text-xl font-bold text-gray-800 dark:text-white">
-            @lang('admin::app.settings.data-transfer.imports.index.title')
-        </p>
+    <div class="flex flex-col gap-4"> 
+        <div class="flex items-center justify-between rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300">
+            <div class="flex flex-col gap-2">
+                <div class="flex cursor-pointer items-center">
+                    {!! view_render_event('admin.settings.data_transfers.index.breadcrumbs.before') !!}
 
-        <div class="flex items-center gap-x-2.5">
-            <!-- Create New Tax Rate Button -->
-            @if (bouncer()->hasPermission('settings.data_transfer.imports.create'))
-                <a href="{{ route('admin.settings.data_transfer.imports.create') }}" class="primary-button">
-                    @lang('admin::app.settings.data-transfer.imports.index.button-title')
-                </a>
-            @endif
+                    <!-- Breadcrumbs -->
+                    <x-admin::breadcrumbs name="settings.data_transfers" />
+
+                    {!! view_render_event('admin.settings.data_transfers.index.breadcrumbs.after') !!}
+                </div>
+
+                <div class="text-xl font-bold dark:text-white">
+                    @lang('admin::app.settings.data-transfer.imports.index.title')
+                </div>
+            </div>
+
+            <div class="flex items-center gap-x-2.5">
+                <!-- Create button for person -->
+                <div class="flex items-center gap-x-2.5">
+                    {!! view_render_event('admin.settings.data_transfers.index.create_button.before') !!}
+
+                    @if (bouncer()->hasPermission('settings.data_transfer.imports.create'))
+                        <a 
+                            href="{{ route('admin.settings.data_transfer.imports.create') }}" 
+                            class="primary-button"
+                        >
+                            @lang('admin::app.settings.data-transfer.imports.index.button-title')
+                        </a>
+                    @endif
+
+                    {!! view_render_event('admin.settings.data_transfers.index.create_button.after') !!}
+                </div>
+            </div>
         </div>
-    </div>
 
-    <x-admin::datagrid :src="route('admin.settings.data_transfer.imports.index')"/>
+        {!! view_render_event('admin.settings.data_transfers.index.datagrid.before') !!}
+
+        <!-- DataGrid -->
+        <x-admin::datagrid :src="route('admin.settings.data_transfer.imports.index')">
+            <!-- DataGrid Shimmer -->
+            <x-admin::shimmer.datagrid />
+        </x-admin::datagrid>
+
+        {!! view_render_event('admin.settings.data_transfers.index.datagrid.after') !!}
+    </div>
 </x-admin::layouts>
