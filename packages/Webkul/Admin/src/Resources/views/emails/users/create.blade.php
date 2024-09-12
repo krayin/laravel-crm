94bcddf15a09d39d1702ab25d981914ac296c89b
suraj kashyap <suraj.kashyap370@webkul.in>
2024-09-12 13:26:16 +0530

    Issue #1598 fixed.

--- packages/Webkul/Admin/src/Resources/views/emails/users/create.blade.php
+++ packages/Webkul/Admin/src/Resources/views/emails/users/create.blade.php
@@ -1,19 +1,11 @@
-@component('admin::emails.layouts.master')
-    <div style="text-align: center;">
-        <a href="{{ config('app.url') }}">
-            <img src="{{ asset('vendor/webkul/admin/assets/images/logo.svg') }}" alt="{{ config('app.name') }}"/>
-        </a>
-    </div>
+@component('admin::emails.layout')
+    <div style="margin-bottom: 34px;">
+        <p style="font-weight: bold;font-size: 20px;color: #121A26;line-height: 24px;margin-bottom: 24px">
+            @lang('admin::app.emails.common.user.dear', ['username' => $user_name]), 👋
+        </p>
 
-    <div style="padding: 30px;">
-        <div style="font-size: 20px;color: #242424;line-height: 30px;margin-bottom: 34px;">
-            <p style="font-size: 16px;color: #5E5E5E;line-height: 24px;">
-                {{ __('admin::app.mail.forget-password.dear', ['name' => $user_name]) }},
-            </p>
-
-            <p style="font-size: 16px;color: #5E5E5E;line-height: 24px;">
-                {{ __('admin::app.mail.user.create-body') }}
-            </p>
-        </div>
+        <p style="font-size: 16px;color: #384860;line-height: 24px;">
+            @lang('admin::app.emails.common.user.create-body')
+        </p>
     </div>
 @endcomponent
