ec02431728e6a23fbbb4ad9079c13b9cf7ee7ae2
Shivendra <shivendra.gupta423@webkul.in>
2024-11-29 19:47:01 +0530

    Update lead table foregin keys

---
+++ packages/Webkul/Lead/src/Database/Migrations/2024_11_29_120302_modify_foreign_keys_in_leads_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->nullable()->change();
+            $table->integer('person_id')->unsigned()->nullable()->change();
+
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['person_id']);
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('restrict');
+
+            $table->foreign('person_id')
+                ->references('id')->on('persons')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['person_id']);
+
+            $table->integer('user_id')->unsigned()->nullable(false)->change();
+            $table->integer('person_id')->unsigned()->nullable(false)->change();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('person_id')
+                ->references('id')->on('persons')
+                ->onDelete('cascade');
+        });
+    }
+};
