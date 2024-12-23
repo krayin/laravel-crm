12fc15ced2fb99106fbb5b1525f49227f97850f8
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-12-23 12:57:15 +0530

    Fixed Issue #1828

--- packages/Webkul/Admin/src/Resources/views/products/view/inventory.blade.php
+++ packages/Webkul/Admin/src/Resources/views/products/view/inventory.blade.php
@@ -145,6 +145,7 @@
                     <!-- Edit Drawer -->
                     <x-admin::drawer
                         ref="assignLocationDrawer"
+                        width="600px"
                         class="text-left"
                     >
                         <!-- Drawer Header -->
@@ -197,11 +198,11 @@
                             @lang('admin::app.products.view.inventory.location')
                         </x-admin::table.th>
                         
-                        <x-admin::table.th>
+                        <x-admin::table.th class="!w-[150px]">
                             @lang('admin::app.products.view.inventory.in-stock')
                         </x-admin::table.th>
 
-                        <x-admin::table.th>
+                        <x-admin::table.th class="!w-[150px]">
                             @lang('admin::app.products.view.inventory.allocated')
                         </x-admin::table.th>
 
