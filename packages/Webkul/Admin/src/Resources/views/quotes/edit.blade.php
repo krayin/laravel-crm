5ef6ae22b5f147e8c35527907ab79b4b6e5b2565
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-12-24 17:31:10 +0530

    Fixed Issue #1835

--- packages/Webkul/Admin/src/Resources/views/quotes/edit.blade.php
+++ packages/Webkul/Admin/src/Resources/views/quotes/edit.blade.php
@@ -41,7 +41,9 @@
                 </div>
             </div>
 
-            <v-quote :errors="errors"></v-quote>
+            <v-quote :errors="errors">
+                <x-admin::shimmer.quotes />
+            </v-quote>
         </div>
     </x-admin::form>
 
@@ -306,7 +308,7 @@
 
             <!-- Add New Qoute Item -->
             <span
-                class="self-start text-md cursor-pointer font-semibold text-brandColor hover:underline dark:text-brandColor"
+                class="text-md cursor-pointer self-start font-semibold text-brandColor hover:underline dark:text-brandColor"
                 @click="addProduct"
             >
                 @lang('admin::app.quotes.create.add-item')
