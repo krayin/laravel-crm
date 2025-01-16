4c7b5099f2b15055ca11c4e60231657f0915fb63
Shivendra <shivendra.gupta423@webkul.in>
2025-01-16 16:30:59 +0530

    Fixed issue #1954

---
+++ public/admin/build/assets/vi.es-c0302793.js
@@ -0,0 +1,5 @@
+/**
+  * vue-cal v4.9.0
+  * (c) 2024 Antoni Andre <antoniandre.web@gmail.com>
+  * @license MIT
+  */const a=["Thứ hai","Thứ ba","Thứ tư","Thứ năm","Thứ sáu","Thứ bảy","Chủ nhật"],E=["T2","T3","T4","T5","T6","T7","CN"],e=["Tháng 1","Tháng 2","Tháng 3","Tháng 4","Tháng 5","Tháng 6","Tháng 7","Tháng 8","Tháng 9","Tháng 10","Tháng 11","Tháng 12"],o="Năm",n="Năm nay",h="Tháng",t="Tuần",T="Ngày",s="Hôm nay",x="NKhông có Event",u="Cả ngày",d="Xóa",y="Tạo event",g="dddd MMMM D YYYY",m={weekDays:a,weekDaysShort:E,months:e,years:"Năm",year:n,month:h,week:t,day:T,today:s,noEvent:x,allDay:u,deleteEvent:"Xóa",createEvent:y,dateFormat:g};export{u as allDay,y as createEvent,g as dateFormat,T as day,m as default,d as deleteEvent,h as month,e as months,x as noEvent,s as today,t as week,a as weekDays,E as weekDaysShort,n as year,o as years};
