126fb274f19b17ec62ac84552649520936f91cf5
Shivendra <shivendra.gupta423@webkul.in>
2025-01-22 18:16:30 +0530

    Done enhancemet #1984

---
+++ public/admin/build/assets/he.es-38780c8b.js
@@ -0,0 +1,5 @@
+/**
+  * vue-cal v4.9.0
+  * (c) 2024 Antoni Andre <antoniandre.web@gmail.com>
+  * @license MIT
+  */const u=["שני","שלישי","רביעי","חמישי","שישי","שבת","ראשון"],D=["ינואר","פברואר","מרץ","אפריל","מאי","יוני","יולי","אוגוסט","ספטמבר","אוקטובר","נובמבר","דצמבר"],E="שנים",r="שנה",a="חודש",e="שבוע",l="יום",t="היום",s="אין אירועים",d="כל היום",n="מחיקה",o="צור אירוע",y="dddd D MMMM YYYY",m={weekDays:u,months:D,years:E,year:"שנה",month:a,week:e,day:"יום",today:t,noEvent:s,allDay:d,deleteEvent:n,createEvent:o,dateFormat:y};export{d as allDay,o as createEvent,y as dateFormat,l as day,m as default,n as deleteEvent,a as month,D as months,s as noEvent,t as today,e as week,u as weekDays,r as year,E as years};
