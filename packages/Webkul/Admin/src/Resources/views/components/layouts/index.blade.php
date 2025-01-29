6deb58d5dfbc499a1024a1eddee68d254fec756b
amit kumar laravel <amitkumar.laravel358@webkul.in>
2025-01-29 13:42:43 +0530

    feat: update footer label rendering to support HTML and add migration to change column type in core_config table

--- packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
@@ -125,7 +125,7 @@
                 <!-- Powered By -->
                 <div class="mt-auto pt-6">
                     <div class="border-t bg-white py-5 text-center text-sm font-normal dark:border-gray-800 dark:bg-gray-900 dark:text-white">
-                        <p>{{ core()->getConfigData('general.settings.footer.label') }}</p>
+                        <p>{!! core()->getConfigData('general.settings.footer.label') !!}</p>
                     </div>
                 </div>
             </div>
