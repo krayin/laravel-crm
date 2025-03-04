10581c92c78a0e5dede76a5bb0647ea19e8d725e
Shivendra <shivendra.gupta423@webkul.in>
2025-03-04 18:08:14 +0530

    Added datagrid card view functionality and fix email datagrid

--- packages/Webkul/Admin/src/Resources/views/components/shimmer/datagrid/table/head.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/shimmer/datagrid/table/head.blade.php
@@ -15,7 +15,7 @@
         <div class="shimmer h-[17px] w-[100px] place-self-end"></div>
     </div>
 @else
-    <div class="row tems-center grid grid-cols-[2fr_1fr_1fr] items-center gap-2.5 border-b px-4 py-2.5 dark:border-gray-800">
+    <div class="row grid grid-cols-[2fr_1fr_1fr] items-center gap-2.5 border-b px-4 py-2.5 dark:border-gray-800">
         <div class="flex items-center gap-2.5">
             <div class="shimmer h-6 w-6"></div>
 
--- packages/Webkul/Admin/src/Resources/views/components/shimmer/leads/index/kanban/toolbar.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/shimmer/leads/index/kanban/toolbar.blade.php
@@ -1,10 +1,10 @@
-<div class="flex justify-between">
+<div class="flex justify-between gap-2 max-md:flex-wrap">
     <div class="flex w-full items-center gap-x-1.5">
         <div class="shimmer light-shimmer-bg h-[38px] w-[264px] rounded-md"></div>
         <div class="shimmer light-shimmer-bg h-9 w-[62px] rounded-md"></div>
     </div>
 
-    <div class="flex gap-4">
+    <div class="flex items-center gap-4 max-md:w-full max-md:!justify-between">
         <!-- Pipeline Switcher -->
         <div class="shimmer light-shimmer-bg h-10 w-[172px] rounded-md"></div>
 
