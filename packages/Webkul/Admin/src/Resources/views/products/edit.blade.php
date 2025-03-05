4a04f8e1056ddfd6cd73802d4dac164f721f747e
Shivendra <shivendra.gupta423@webkul.in>
2025-03-05 13:09:44 +0530

    Fixed attribue listing in product

--- packages/Webkul/Admin/src/Resources/views/products/edit.blade.php
+++ packages/Webkul/Admin/src/Resources/views/products/edit.blade.php
@@ -58,8 +58,7 @@
                         <x-admin::attributes
                             :custom-attributes="app('Webkul\Attribute\Repositories\AttributeRepository')->findWhere([
                                 'entity_type' => 'products',
-                                ['code', 'NOTIN', ['price']],
-                                ['code' , '!=', 'quantity']
+                                ['code', 'NOTIN', ['price', 'quantity']],
                             ])"
                             :entity="$product"
                         />
@@ -91,8 +90,7 @@
                             <x-admin::attributes
                                 :custom-attributes="app('Webkul\Attribute\Repositories\AttributeRepository')->findWhere([
                                     'entity_type' => 'products',
-                                    ['code', 'IN', ['price']],
-                                    ['code' , '!=', 'quantity']
+                                    ['code', 'IN', ['price', 'quantity']],
                                 ])"
                                 :entity="$product"
                             />
