d403127bf3b5dbd866ad5e8fad4d50b793b13253
sagarKumar <sagarkumar.qa@webkul.in>
2025-04-14 14:49:58 +0530

    written script for attribut management

---
+++ packages/Webkul/Admin/tests/e2e-pw/tests/settings/automation/attribute.spec.ts
@@ -0,0 +1,90 @@
+import { test } from "../../../setup";
+import { generateFirstName, generateFullName, generateSKU } from "../../../utils/faker";
+
+test.describe("attribute management", async () => {
+
+    test("should create an attribute", async ({ adminPage }) => {
+
+        /**
+         * Reaching to the attribute listing page.
+         */
+        await adminPage.goto("admin/settings/attributes");
+
+        /**
+         * Opening create attribute form in modal.
+         */
+        await adminPage.getByRole('link', { name: 'Create Attribute' }).click();
+
+        /**
+         * Filling the form with attribute details.
+         */
+
+        await adminPage.getByRole('textbox', { name: 'Name' }).click();
+        await adminPage.getByRole('textbox', { name: 'Name' }).fill(generateFullName());
+        await adminPage.getByRole('textbox', { name: 'Code' }).click();
+        await adminPage.getByRole('textbox', { name: 'Code' }).fill(generateSKU());
+        await adminPage.getByRole('textbox', { name: 'Code' }).press('Tab');
+        await adminPage.locator('#type').selectOption('text');
+        await adminPage.locator('#entity_type').selectOption('persons');
+        await adminPage.locator('#validation').selectOption('numeric');
+
+        /**
+         * Save attribute and close the modal.
+         */
+        await adminPage.getByRole('button', { name: 'Save Attribute' }).click();
+
+
+    })
+    test("should edit an attribute", async ({ adminPage }) => {
+        /**
+         * Reaching to the attribute listing page.
+         */
+        await adminPage.goto("admin/settings/attributes");
+
+
+        /**
+         * Clicking on the edit button for the first attribute opens the modal.
+         */
+        await adminPage.locator('//span[@class="cursor-pointer rounded-md p-1.5 text-2xl transition-all hover:bg-gray-200 dark:hover:bg-gray-800 max-sm:place-self-center icon-edit"]').first().click();
+
+
+        /**
+         * Fill the form with the attribute details.
+         */
+        await adminPage.getByRole('textbox', { name: 'Name' }).click();
+        await adminPage.getByRole('textbox', { name: 'Name' }).fill(generateFullName());
+        await adminPage.getByRole('textbox', { name: 'Code' }).press('Tab');
+        await adminPage.locator('#entity_type').selectOption('persons');
+
+        /**
+         * Save attribute and close the modal.
+         */
+        await adminPage.getByRole('button', { name: 'Save Attribute' }).click();
+
+    })
+    test("should delete an attribute", async ({ adminPage }) => {
+        /**
+         * Reaching to the attribute listing page.
+         */
+        await adminPage.goto("admin/settings/attributes");
+
+
+        /**
+         * Clicking on the delete button for the first attribute opens the modal.
+         */
+        await adminPage.locator('//span[@class="cursor-pointer rounded-md p-1.5 text-2xl transition-all hover:bg-gray-200 dark:hover:bg-gray-800 max-sm:place-self-center icon-delete"]').first().click();
+        /**
+         * Delete confirmation modal.
+         */
+        await adminPage.getByRole('button', { name: 'Agree', exact: true }).click();
+
+
+        /**
+         * Checking if the attribute is deleted successfully.
+         */
+
+        await adminPage.getByText('Attribute deleted successfully.').isVisible();
+
+    })
+
+})
