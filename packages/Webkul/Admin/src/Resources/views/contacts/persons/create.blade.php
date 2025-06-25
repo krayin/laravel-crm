d5048178c9d7bf4311d8ebfbf4074eda7fba1ad0
Alireza Mohammadi Arani <alireza2756@gmail.com>
2025-06-25 13:05:19 +0400

    update

--- packages/Webkul/Admin/src/Resources/views/contacts/persons/create.blade.php
+++ packages/Webkul/Admin/src/Resources/views/contacts/persons/create.blade.php
@@ -46,11 +46,86 @@
             <!-- Form fields -->
             <div class="box-shadow rounded-lg border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-gray-900">
                 {!! view_render_event('admin.persons.create.form_controls.before') !!}
+                @php
+                    $attributes = app('Webkul\Attribute\Repositories\AttributeRepository')->findWhere([
+                    'entity_type' => 'persons',
+                ]);
+
+                 $should_remove_fields=[];
+
+                foreach ($attributes as $attribute){
+                    if($attribute->code==='person_type'){
+
+                           $attributeValues = app('Webkul\Attribute\Repositories\AttributeValueRepository')->findWhere([
+                                    'entity_type' => 'persons',
+                                    'attribute_id'=>$attribute->id
+                                ])->first();
+
+
+                            switch($attributeValues->integer_value){
+                                case 1:
+
+                                break;
+                                 case 2:
+
+                                $should_remove_fields=['company_name_en','company_name_ar','license_no',
+                                'company_issue_date','company_expiry_date','partner_2','partner_3','local_agent'
+                                ];
+
+                                break;
+                                 case 3:
+                 $should_remove_fields=['company_name_en','company_name_ar','license_no',
+                                'company_issue_date','company_expiry_date','partner_2','partner_3','local_agent'
+                                ];
+
+                                break;
+
+                            }
+
+                    }
+                }
+
+
+                array_push($should_remove_fields,'rate');
+
+                $rate=0;
+
+
+
+
+
+                //dump($attributeValues);
+
+                $attributes = $attributes->reject(function ($attribute) use ($should_remove_fields) {
+                    return in_array($attribute->code, $should_remove_fields);
+                });
+
+                $rate = intval($rate);
+
+                @endphp
+
+                <div class="mb-4 mb-2.5 w-full" style="display: inline-table">
+                    <div class="person-rate">
+                        <input type="radio" <?php echo $rate == 5 ? "checked" : ""; ?> id="star5" name="rate"
+                               value="5"/>
+                        <label for="star5" title="text">5 stars</label>
+                        <input type="radio" <?php echo $rate == 4 ? "checked" : ""; ?> id="star4" name="rate"
+                               value="4"/>
+                        <label for="star4" title="text">4 stars</label>
+                        <input type="radio" <?php echo $rate == 3 ? "checked" : ""; ?> id="star3" name="rate"
+                               value="3"/>
+                        <label for="star3" title="text">3 stars</label>
+                        <input type="radio" <?php echo $rate == 2 ? "checked" : ""; ?> id="star2" name="rate"
+                               value="2"/>
+                        <label for="star2" title="text">2 stars</label>
+                        <input type="radio" <?php echo $rate == 1 ? "checked" : ""; ?> id="star1" name="rate"
+                               value="1"/>
+                        <label for="star1" title="text">1 star</label>
+                    </div>
+                </div>
 
                 <x-admin::attributes
-                    :custom-attributes="app('Webkul\Attribute\Repositories\AttributeRepository')->findWhere([
-                        'entity_type' => 'persons',
-                    ])"
+                    :custom-attributes="$attributes"
                     :custom-validations="[
                         'name' => [
                             'min:2',
