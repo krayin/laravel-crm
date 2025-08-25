a554304c767eb1ac4f9c1f61f6a933e4be3b6c4a
Aurélien Lemaire <aurelien@etayage.fr>
2025-08-25 14:24:50 +0200

    [MIN] remove AppServiceProvider boot evol

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (str_starts_with(config('app.url'), 'https')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        //
     }
 }
