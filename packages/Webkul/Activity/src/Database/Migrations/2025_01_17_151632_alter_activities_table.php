bd3357d765cae3467eed15ac33ff9b6a9b5e47ab
vikas vishwakarma <vikas.vishwakarma749@webkul.com>
2025-09-11 17:09:02 +0530

    pull updated code

--- packages/Webkul/Activity/src/Database/Migrations/2025_01_17_151632_alter_activities_table.php
+++ packages/Webkul/Activity/src/Database/Migrations/2025_01_17_151632_alter_activities_table.php
@@ -36,17 +36,17 @@ return new class extends Migration
             DB::statement('SET FOREIGN_KEY_CHECKS=0');
 
             // Drop the foreign key constraint using raw SQL.
-            DB::statement('ALTER TABLE '.$tablePrefix.'activities DROP FOREIGN KEY '.$tablePrefix.'activities_user_id_foreign');
+            DB::statement('ALTER TABLE '. $tablePrefix .'activities DROP FOREIGN KEY '. $tablePrefix .'activities_user_id_foreign');
 
             // Drop the index.
-            DB::statement('ALTER TABLE '.$tablePrefix.'activities DROP INDEX '.$tablePrefix.'activities_user_id_foreign');
+            DB::statement('ALTER TABLE '. $tablePrefix .'activities DROP INDEX '. $tablePrefix .'activities_user_id_foreign');
 
             // Change the column to be non-nullable.
             $table->unsignedInteger('user_id')->nullable(false)->change();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             // Re-enable foreign key checks.
-            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+            DB::statement('SET FOREIGN_KEY_CHECKS=1'); 
         });
     }
 };
