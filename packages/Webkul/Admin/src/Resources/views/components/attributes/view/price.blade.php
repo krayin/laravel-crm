3341710e480b240bbb284426857355bd834fb92a
bhavesh <BhaveshLakhwani@gmail.com>
2026-02-24 00:42:38 +0530

    fix: improve lead value readability by adding comma-separated number formatting

--- packages/Webkul/Admin/src/Resources/views/components/attributes/view/price.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/attributes/view/price.blade.php
@@ -1,9 +1,21 @@
+@php
+    $isLeadValue = $attribute->entity_type === 'leads'
+        && $attribute->code === 'lead_value';
+
+    $displayLabel = $value;
+
+    if ($isLeadValue && is_numeric($value)) {
+        $displayLabel = number_format((float) $value, 0);
+    }
+@endphp
+
 <x-admin::form.control-group.controls.inline.text
     type="inline"
     ::name="'{{ $attribute->code }}'"
-    ::value="'{{ $value }}'"
+    :value="$value ?? ''"
+    :value-label="$value == '' ? '--' : $displayLabel"
     position="left"
-    rules="required"
+    rules="required|{{ $attribute->validation }}"
     :label="$attribute->name"
     :placeholder="$attribute->name"
     ::errors="errors"
