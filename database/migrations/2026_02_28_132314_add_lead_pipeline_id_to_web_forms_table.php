c8ddbfbb2340c89b3b32301bb35a87d6ffde8592
bhavesh <BhaveshLakhwani@gmail.com>
2026-02-28 14:11:49 +0530

    feat: add lead_pipeline_id column to web_forms table

---
+++ database/migrations/2026_02_28_132314_add_lead_pipeline_id_to_web_forms_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('web_forms', function (Blueprint $table) {
+            $table->unsignedInteger('lead_pipeline_id')
+              ->nullable()
+              ->after('create_lead');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('web_forms', function (Blueprint $table) {
+            $table->dropColumn('lead_pipeline_id');
+        });
+    }
+};
